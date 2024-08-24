66d5caedae9aa1cba6510511bf3564cdb6d03109
Pew <hi@pew.dev>
2024-08-24 10:58:27 +0800

    Add basic structure and footer content

---
+++ src/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://pew.dev/">
+            Pew
+          </a>
+          .
+        </p>
+      </small>
+    </footer>
+  );
+}
