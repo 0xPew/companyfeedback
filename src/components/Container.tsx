aaeaf702f43465fddcc11378e6c89effe99b0f39
Pew <hi@pew.dev>
2024-08-26 14:06:45 +0800

    Update structure

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import FeedbackList from "./Feedbacklist";
+import Header from "./Header";
+
+export default function Container() {
+  return (
+    <main className="container">
+      <Header />
+      <FeedbackList />
+    </main>
+  );
+}
---
+++ src/components/HashtagList.tsx
@@ -0,0 +1,12 @@
+export default function HashtagList() {
+  return (
+    <ul className="hashtags">
+      <li>
+        <button>#Pew</button>
+      </li>
+      <li>
+        <button>#PewPew</button>
+      </li>
+    </ul>
+  );
+}
