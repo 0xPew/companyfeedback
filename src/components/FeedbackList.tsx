b7151e9a6142ee033443bb2dd730c860a1bcd58b
Pew <hi@pew.dev>
2024-09-01 07:05:19 +0800

    Add feedback list placeholder content

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,25 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 export default function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>543</span>
+        </button>
+        <div>
+          <p>P</p>
+        </div>
+        <div>
+          <p>Pew</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rem
+            laboriosam, architecto sit blanditiis dolor?
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 }
