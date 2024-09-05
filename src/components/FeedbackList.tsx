294f8f84ea17f07976726637650588f1ff1b39cc
Pew <hi@pew.dev>
2024-09-05 21:23:53 +0800

    Add feedback item component

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,25 +1,28 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import FeedbackItem from "./FeedbackItem";
+
+const feedbackItems = [
+  {
+    upvoteCount: 234,
+    badgeLetter: "P",
+    companyName: "Pew",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rem laboriosam, architecto sit blanditiis dolor?",
+    daysAgo: 4,
+  },
+  {
+    upvoteCount: 143,
+    badgeLetter: "Z",
+    companyName: "Zus",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, voluptate a tempora repellat adipisci sequi.",
+    daysAgo: 7,
+  },
+];
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>543</span>
-        </button>
-        <div>
-          <p>P</p>
-        </div>
-        <div>
-          <p>Pew</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rem
-            laboriosam, architecto sit blanditiis dolor?
-          </p>
-        </div>
-        <p>4d</p>
-      </li>
+      {feedbackItems.map((feedbackItem, index) => (
+        <FeedbackItem key={index} feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
