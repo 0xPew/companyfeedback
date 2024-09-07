b2e1044543125aed9c105c053ad5d3c28d5b3b4f
Pew <hi@pew.dev>
2024-09-07 22:15:01 +0800

    Update FeedbackList component to fetch feedback data from API

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,27 +1,25 @@
+import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 
-const feedbackItems = [
-  {
-    upvoteCount: 234,
-    badgeLetter: "P",
-    companyName: "Pew",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rem laboriosam, architecto sit blanditiis dolor?",
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 143,
-    badgeLetter: "Z",
-    companyName: "Zus",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, voluptate a tempora repellat adipisci sequi.",
-    daysAgo: 7,
-  },
-];
-
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbackItems(data.feedbacks);
+      });
+  }, []);
+
   return (
     <ol className="feedback-list">
       {feedbackItems.map((feedbackItem, index) => (
-        <FeedbackItem key={index} feedbackItem={feedbackItem} />
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
     </ol>
   );
